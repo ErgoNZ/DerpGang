233489bf19399a0ef01a7ac34ceab3d63d679ce2
Seth Wagener <sethwagener@gmail.com>
2023-08-03 11:01:35 +1200

    Solved the inventory dilemma

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class MenuLogic : MonoBehaviour
 {
-    GameObject Party, Inventory, Map, Insights, Journal, Canvas;
+    GameObject Party, Inv, Map, Insights, Journal, Canvas;
     Vector3 PartyPos, InvPos, MapPos, InsightsPos;
     PlayerData PData;
+    string InvFilter = "Consumables";
     // Start is called before the first frame update
     private void Start()
     {
         Party = GameObject.Find("PartyBtn");
-        Inventory = GameObject.Find("InvBtn");
+        Inv = GameObject.Find("InvBtn");
         Map = GameObject.Find("MapBtn");
         Insights = GameObject.Find("InsightBtn");
         Journal = GameObject.Find("JournalBtn");
         Canvas = GameObject.Find("Canvas");
         PData = GetComponent<PlayerData>();
         PartyPos = Party.transform.localPosition;
-        InvPos = Inventory.transform.localPosition;
+        InvPos = Inv.transform.localPosition;
         MapPos = Map.transform.localPosition;
         InsightsPos = Insights.transform.localPosition;
         Canvas.SetActive(false);
@@ -35,54 +36,49 @@ public class MenuLogic : MonoBehaviour
             case "Party":
                 Debug.LogWarning("Party");
                 Party.transform.localPosition = PartyPos;
-                Inventory.transform.localPosition = InvPos;
+                Inv.transform.localPosition = InvPos;
                 Map.transform.localPosition = MapPos;
                 Insights.transform.localPosition = InsightsPos;
                 break;
             case "Inventory":
                 Debug.LogWarning("Inventory");
                 Party.transform.localPosition = new Vector3(PartyPos.x - 172.80f, PartyPos.y);
-                Inventory.transform.localPosition = InvPos;
+                Inv.transform.localPosition = InvPos;
                 Map.transform.localPosition = MapPos;
                 Insights.transform.localPosition = InsightsPos;
-                for (int i = 0; i < PData.Inventory.Count; i++)
-                {
-                    string line;
-                    line = PData.Inventory[i].Name + " : " + PData.Inventory[i].Amount;
-                }
                 break;
             case "Map":
                 Debug.LogWarning("Map");
                 Party.transform.localPosition = new Vector3(PartyPos.x - 172.80f, PartyPos.y);
-                Inventory.transform.localPosition = new Vector3(InvPos.x - 172.80f * 3, InvPos.y);
+                Inv.transform.localPosition = new Vector3(InvPos.x - 172.80f * 3, InvPos.y);
                 Map.transform.localPosition = MapPos;
                 Insights.transform.localPosition = InsightsPos;
                 break;
             case "Insights":
                 Debug.LogWarning("Insights");
                 Party.transform.localPosition = new Vector3(PartyPos.x - 172.80f, PartyPos.y);
-                Inventory.transform.localPosition = new Vector3(InvPos.x - 172.80f * 3, InvPos.y);
+                Inv.transform.localPosition = new Vector3(InvPos.x - 172.80f * 3, InvPos.y);
                 Map.transform.localPosition = new Vector3(MapPos.x - 172.80f * 5, MapPos.y);
                 Insights.transform.localPosition = InsightsPos;
                 break;
             case "Journal":
                 Debug.LogWarning("Journal");
                 Party.transform.localPosition = new Vector3(PartyPos.x - 172.80f, PartyPos.y);
-                Inventory.transform.localPosition = new Vector3(InvPos.x - 172.80f * 3, InvPos.y);
+                Inv.transform.localPosition = new Vector3(InvPos.x - 172.80f * 3, InvPos.y);
                 Map.transform.localPosition = new Vector3(MapPos.x - 172.80f * 5, MapPos.y);
                 Insights.transform.localPosition = new Vector3(InsightsPos.x - 172.80f * 7, InsightsPos.y);
                 break;
             case "Item":
-
+                InvFilter = "Consumables";
                 break;
             case "Gear":
-
+                InvFilter = "Gear";
                 break;
             case "Charms":
-
+                InvFilter = "Charms";
                 break;
             case "Key":
-
+                InvFilter = "Key";
                 break;
             default:
                 Debug.LogWarning("Default");
